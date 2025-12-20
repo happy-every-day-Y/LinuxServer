3ee213b1c85674f60dd817eaa7e631c909d6e5a7
快乐每一天 <1252982977@qq.com>
2025-12-20 21:38:23 +0800

    remove build and bin artifacts from repository

--- build/apps/chatd/CMakeFiles/chatd.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for chatd.
-# This may be replaced when dependencies are built.
--- build/src/base/thread_pool/CMakeFiles/thread_pool.dir/Thread_pool.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/base/thread_pool/CMakeFiles/thread_pool.dir/Thread_pool.cpp.o: \
- /home/zhifei/LinuxServer/src/base/thread_pool/Thread_pool.cpp \
- /usr/include/stdc-predef.h
